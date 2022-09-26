082bdeb08fb88307a6d5d9cfa7f8ce9b228d27ca
princeabdul90 <abuabdu99@gmail.com>
2022-09-26 19:23:17 +0800

    deleted script

--- lib/utils/config.dart
+++
@@ -1,9 +0,0 @@
-/*
-* Developer: Abubakar Abdullahi
-* Date: 21/09/2022
-*/
-
-class Config {
-  static const appIcon = "assets/loving.png";
-  static const loginIcon = "assets/cherry-ontop.png";
-}
